8ebaa2b50ee39fd419a07872b72fa60fbd51348c
chrisw <chrisw@soe.ucsc.edu>
2013-12-22 07:59:46 -0800

    set global chart options

--- scripts/cohortData.js
+++ scripts/cohortData.js
@@ -13,37 +13,58 @@ console.log(patient);
 
 var ids = cohort.getPatientIds();
 
-for (var i in ids) {
-    var id = ids[i];
-    console.log(i + ": " + id);
-    var p = cohort.getPatient(id);
-    if (p) {
-        console.log("study: " + p.getStudySite());
-        console.log("biopsy: " + p.getBiopsySite());
-    }
-}
+// for (var i in ids) {
+// var id = ids[i];
+// console.log(i + ": " + id);
+// var p = cohort.getPatient(id);
+// if (p) {
+// console.log("study: " + p.getStudySite());
+// console.log("biopsy: " + p.getBiopsySite());
+// }
+// }
 
-var chartOptions = {
+Highcharts.setOptions({
     chart : {
-        type : 'bar'
+        backgroundColor : {
+            linearGradient : [0, 0, 500, 500],
+            stops : [[0, 'rgb(255, 255, 255)'], [1, 'rgb(240, 240, 255)']]
+        },
+        borderWidth : 2,
+        plotBackgroundColor : 'rgba(255, 255, 255, .9)',
+        plotShadow : true,
+        plotBorderWidth : 1
+    }
+});
+
+var chart1Options = {
+    chart : {
+
     },
-    title : {
-        text : 'Fruit Consumption'
-    },
+
     xAxis : {
-        categories : ['Apples', 'Bananas', 'Oranges']
+        type : 'datetime'
     },
-    yAxis : {
-        title : {
-            text : 'Fruit eaten'
-        }
-    },
+
     series : [{
-        name : 'Jane',
-        data : [1, 0, 40]
-    }, {
-        name : 'John',
-        data : [5, 7, 3]
+        data : [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+        pointStart : Date.UTC(2010, 0, 1),
+        pointInterval : 3600 * 1000 // one hour
+    }]
+};
+
+var chart2Options = {
+    chart : {
+        type : 'column'
+    },
+
+    xAxis : {
+        type : 'datetime'
+    },
+
+    series : [{
+        data : [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+        pointStart : Date.UTC(2010, 0, 1),
+        pointInterval : 3600 * 1000 // one hour
     }]
 };
 
@@ -54,7 +75,8 @@ function addHighChart(elementId, chartOptions) {
     });
 }
 
-addHighChart("chart", chartOptions);
+addHighChart("chart01", chart1Options);
+addHighChart("chart02", chart2Options);
 
 // TODO objects below
 
